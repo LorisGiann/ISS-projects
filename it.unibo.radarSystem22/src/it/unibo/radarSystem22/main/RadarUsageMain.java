2428b73bbe6329085e9686ee067316d102b05579
Loris Giann <loris.giann@live.it>
2022-03-03 22:31:30 +0100

    radar client-server udp system

--- it.unibo.radarSystem22/src/it/unibo/radarSystem22/main/RadarUsageMain.java
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/main/RadarUsageMain.java
@@ -1,13 +1,22 @@
 package it.unibo.radarSystem22.main;
+import java.util.concurrent.TimeUnit;
+
 import radarPojo.radarSupport;
 
 public class RadarUsageMain {
-    public void doJob() {
+    public void doJob() throws Exception {
             System.out.println("start");
             radarSupport.setUpRadarGui();
-            radarSupport.update( "40", "60");
+            ConnClient c = new ConnClient("192.168.43.167");
+            while(true) {
+            	TimeUnit.MILLISECONDS.sleep(500);
+            	String repl = c.sendReq();
+            	System.out.println(repl);
+            	radarSupport.update( repl, "60");
+            }
+            
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
             new RadarUsageMain().doJob();
     }
 }
