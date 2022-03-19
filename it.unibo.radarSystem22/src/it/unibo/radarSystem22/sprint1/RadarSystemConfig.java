5260df9e82e7e8b5e6b59fde4f94b902c1b7705a
Loris Giann <loris.giann@live.it>
2022-03-19 18:39:38 +0100

    comm2022

--- it.unibo.radarSystem22/src/it/unibo/radarSystem22/sprint1/RadarSystemConfig.java
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/sprint1/RadarSystemConfig.java
@@ -12,16 +12,23 @@ import it.unibo.radarSystem22.domain.utils.ColorsOut;
 public class RadarSystemConfig {
  	public static boolean tracing         = false;	
 	public static boolean testing         = false;			
-	public static int DLIMIT              =  15;     
-	
+	public static int DLIMIT              =  15;  
 	public static boolean  RadarGuiRemote = false;
+	
+//Aggiunte dello SPRINT2	
+ 	public static String hostAddr         = "localhost";		
+	public static String raspAddr         = "localhost";		
+	public static int serverPort          = 8080;
+//Aggiunte dello SPRINT2a 	
+	public static int ledPort             = 8010;
+	public static int sonarPort           = 8015;
 	
 	public static void setTheConfiguration(  ) {
 		setTheConfiguration("../RadarSystemConfig.json");
 	}
 	
 	public static void setTheConfiguration( String resourceName ) {
-		//Nella distribuzione resourceName è in una dir che include la bin  
+		//Nella distribuzione resourceName ï¿½ in una dir che include la bin  
 		FileInputStream fis = null;
 		try {
 			ColorsOut.out("%%% setTheConfiguration from file:" + resourceName);
@@ -35,6 +42,8 @@ public class RadarSystemConfig {
 	        testing          = object.getBoolean("testing");
 	        RadarGuiRemote   = object.getBoolean("RadarGuiRemote");
 	        DLIMIT           = object.getInt("DLIMIT");	
+	        hostAddr         = object.getString("hostAddr");
+	        serverPort       = object.getInt("serverPort");
  	        
 		} catch (FileNotFoundException e) {
  			ColorsOut.outerr("setTheConfiguration ERROR " + e.getMessage() );
